c7f4b37e628dca5ee75ba1855b6ec2d9cb924b1d
Benjamin Van Treese <vantreeseba@gmail.com>
2023-09-27 15:12:53 -0500

    fix: Fix issues with ts

--- src/app/npc/page.tsx
+++ src/app/npc/page.tsx
@@ -15,14 +15,14 @@ export default function NPC() {
   return <div className="grid gap-4 mt-4 grid-cols-2">{npcs.map(toNpcCard)}</div>;
 }
 
-function toNpcCard(npc) {
-  const clothes = npc.clothes.split('\n').map((x) => <li key={x}>{x}</li>);
-  const history = npc.history.split('\n').map((x) => <div key={x}>{x}</div>);
-  const parents = npc.parents.split('\n').map((x) => <div key={x}>{x}</div>);
-  const siblings = npc.siblings.split('\n').map((x) => <div key={x}>{x}</div>);
-  const likes = npc.likes.split('\n').map((x) => <div key={x}>{x}</div>);
-  const traits = npc.traits.split('\n').map((x) => <div key={x}>{x}</div>);
-  const goals = npc.goals.split('\n').map((x) => <div key={x}>{x}</div>);
+function toNpcCard(npc: any) {
+  const clothes = npc.clothes.split('\n').map((x: any) => <li key={x}>{x}</li>);
+  const history = npc.history.split('\n').map((x: any) => <div key={x}>{x}</div>);
+  const parents = npc.parents.split('\n').map((x: any) => <div key={x}>{x}</div>);
+  const siblings = npc.siblings.split('\n').map((x: any) => <div key={x}>{x}</div>);
+  const likes = npc.likes.split('\n').map((x: any) => <div key={x}>{x}</div>);
+  const traits = npc.traits.split('\n').map((x: any) => <div key={x}>{x}</div>);
+  const goals = npc.goals.split('\n').map((x: any) => <div key={x}>{x}</div>);
 
   return (
     <Card>
@@ -45,7 +45,7 @@ function toNpcCard(npc) {
   );
 }
 
-function CollapsibleDivider({ label, children }) {
+function CollapsibleDivider({ label, children }: any) {
   const [isOpen, setIsOpen] = React.useState(true);
 
   return (
@@ -58,7 +58,7 @@ function CollapsibleDivider({ label, children }) {
   );
 }
 
-function Divider({ content, children, onClick }) {
+function Divider({ content, children, onClick }: any) {
   return (
     <Button variant="none" className="relative w-full" onClick={onClick}>
       <div className="absolute inset-0 flex items-center">
